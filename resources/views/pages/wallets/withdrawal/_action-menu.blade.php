2980b74b01efb0d73ad170fc31d60a776cb52743
zetzet7298 <baythanghai@gmail.com>
2023-10-02 23:46:39 +0700

    update

--- resources/views/pages/wallets/withdrawal/_action-menu.blade.php
+++ resources/views/pages/wallets/withdrawal/_action-menu.blade.php
@@ -1,7 +1,9 @@
 <!--begin::Action--->
 <td class="text-end">
+@if($model->content != 'Post Product' && $model->content != 'Update Product')
     <a href="{{ route('wallet.withdrawal.confirm', ['id' => $model->id]) }}" class="btn btn-sm btn-light btn-active-light-primary" onclick="return confirm('Are you sure ?')">
         Confirmed withdrawal
     </a>
+    @endif
 </td>
 <!--end::Action--->
